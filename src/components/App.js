b4dda33582fdd5d7f9de2803c565731baf7a7d78
Ben Chan <benchanviolin@gmail.com>
2017-09-06 07:08:56 -0400

    type: feat
    Used reactstrap to create basic nav bar for categories.

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import '../css/App.css'
 import * as ReadableAPI from '../utils/ReadableAPI'
 import { getCategories } from '../actions'
-import { Button } from 'reactstrap'; //http://reactstrap.github.io/
+import { Navbar, NavbarToggler, NavbarBrand, Collapse, Nav, NavItem, NavLink, Container, Row, Col, Button } from 'reactstrap'; //http://reactstrap.github.io/
 
 class App extends Component {
+  state = {
+    collapsed: true
+  }
   componentDidMount() {
     ReadableAPI.getCategories().then((categories) => {
       this.props.setCategories(categories);
     })
   }
+  toggleNavbar() {
+    this.setState(state => ({
+      collapsed: !state.collapsed
+    }));
+  }
+
   render() {
-    console.log('Props', this.props);
     const { category } = this.props;
 
     return (
-      <div className="nav">
-        <div className="nav-header"></div>
-        <label className="nav-label">Choose a category:</label>
-        {category && category.rows && (category.rows.map((category, key) => {
-          return <Button
-            key={key}
-            className="nav-button"
-            color="primary"
-            size="large"
-          >{category.name}</Button>
-        }))}
+      <div>
+        <Navbar color="faded" light toggleable>
+         <NavbarToggler right onClick={() => {this.toggleNavbar()}} />
+         <NavbarBrand href="/">Click a category on the right!</NavbarBrand>
+         <Collapse isOpen={!this.state.collapsed} navbar>
+           <Nav className="ml-auto" navbar>
+             {category && category.rows && (category.rows.map((category, key) => {
+               return <NavItem key={key}>
+                 <NavLink href={'/'+category.name}>{category.name}</NavLink>
+               </NavItem>
+             }))}
+           </Nav>
+         </Collapse>
+        </Navbar>
+        <Route exact path='/' render={() => (
+          <Container>
+            <Row>
+              <Col>Hi</Col>
+            </Row>
+          </Container>
+        )}/>
+        <Route path='/category' render={() => (
+          <Container></Container>
+        )}/>
       </div>
     );
   }
