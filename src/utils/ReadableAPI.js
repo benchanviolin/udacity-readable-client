024097dcca047f1c96c83a910412b35012d5b3f5
Ben Chan <benchanviolin@gmail.com>
2017-09-06 04:42:35 -0400

    type: feat
    Add ReadableAPI.js.  Remove .env for now (using port 8080 locally for
    testing but should leave 5001 for actual project review).

---
+++ src/utils/ReadableAPI.js
@@ -0,0 +1,11 @@
+const api = process.env.REACT_APP_READABLE_API_URL || 'http://localhost:5001'
+
+let token = localStorage.token
+
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
