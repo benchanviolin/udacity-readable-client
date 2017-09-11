ae6f2fb9378f8c8c9840b22a1543fd9518a8f45b
Ben Chan <benchanviolin@gmail.com>
2017-09-11 06:43:30 -0400

    type: chore
    Suppress logger middleware.

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom'
 import './css/index.css'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, /*applyMiddleware,*/ compose } from 'redux'
 import reducer from './reducers'
 import { Provider } from 'react-redux'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'normalize.css'
 
-const logger = store => next => action => {
+/*const logger = store => next => action => {
   console.group(action.type)
   console.info('dispatching', action)
   let result = next(action)
   console.log('next state', store.getState())
   console.groupEnd(action.type)
   return result
-}
+}*/
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(logger)
+    /*applyMiddleware(logger)*/
   )
 )
 
